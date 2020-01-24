7abce71514594c4378f37464ecc9d23504364500
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-24 00:35:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,6 +9,6 @@ class App < Sinatra::Base
   
   post '/' do
     pgl = PigLatinizer.new
-    @piglatin = pgl.piglatinize(params[:user_input])
+    @piglatin = pgl.piglatinize(params[:user_phrase])
   end
 end
