24dfb01f90b2d0492e81424fe97e6870dff4f070
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-24 00:38:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :user_input 
+  end
+  
+  post '/piglatinize' do
+    pgl = PigLatinizer.new
+    @piglatin = pgl.piglatinize(params[:user_phrase])
+  end
 end
